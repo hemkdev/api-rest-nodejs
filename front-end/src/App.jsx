29ec341179d3ff4dbbc5ff994dbe1877cad12678
hemkdev <lucas.hemkemeier007@gmail.com>
2026-05-03 23:05:01 -0300

    Initial commit

---
+++ front-end/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="bg-slate-500">
+        <h1> Cadastro de usuários</h1>  
+      </div>
+    </>
+  )
+}
+
+export default App
