08b298daf9b0e34f65e570b522e08cddded2f7eb
hemkdev <lucas.hemkemeier007@gmail.com>
2026-05-04 22:35:10 -0300

    chore: adiciona configuração inicial do mongodb

--- back-end/server.js
+++ back-end/server.js
@@ -1,7 +1,27 @@
-import express from 'express'
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose, { mongo } from 'mongoose';
+
+dotenv.config();
 
 const app = express()
+const port = 3000
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log('MongoDB connected')
+  } catch (error) {
+    console.error('MongoDB connection error:', error);
+  }
+}
+
+connectDB();
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
-}
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
